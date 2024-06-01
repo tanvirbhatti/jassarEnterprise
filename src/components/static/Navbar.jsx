3907e9d985bcb0471a485eec2e135847eb8cf053
Tanvirbhatti <Tanvirbhatti682@gmail.com>
2024-06-01 13:44:16 -0400

    navbar menu changes

--- src/components/static/Navbar.jsx
+++ src/components/static/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { Link, useLocation } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom';
 
 const Navbar = () => {
 	const location = useLocation();
@@ -28,54 +28,58 @@ const Navbar = () => {
 				<div className="row justify-content-between">
 					<div className="col-md-12 order-md-last">
 						<div className="row ">
-							<div className="social-media d-flex align-items-center col-md-5">
-								<p className="mb-0 d-flex">
-									<a href="https://www.facebook.com/profile.php?id=100063689943465" target='_blank' className="d-flex align-items-center justify-content-center mr-4"><span
-										className="fa-brands fa-facebook"><i className="sr-only">Facebook</i></span></a>
-									<a href="https://www.instagram.com/jassar_ent_landscping/" target='_blank' className="d-flex align-items-center justify-content-center mr-4"><span
-										className="fa-brands fa-instagram"><i className="sr-only">Twitter</i></span></a>
-									<a href="https://twitter.com/" target='_blank' className="d-flex align-items-center justify-content-center"><span
-										className="fa-brands fa-twitter"><i className="sr-only">Instagram</i></span></a>
+							<div className="social-media d-flex align-items-center col-md-5 col-3">
+								<div className='d-lg-none align-items-center'>
+									<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+										<span className="fa fa-bars"></span>
+									</button>
+								</div>
+								<p className="mb-0 d-none d-lg-flex">
+									<a href="https://www.facebook.com/profile.php?id=100063689943465" target='_blank' className="d-flex align-items-center justify-content-center mr-4">
+										<span className="fa-brands fa-facebook"><i className="sr-only">Facebook</i></span>
+									</a>
+									<a href="https://www.instagram.com/jassar_ent_landscping/" target='_blank' className="d-flex align-items-center justify-content-center mr-4">
+										<span className="fa-brands fa-instagram"><i className="sr-only">Instagram</i></span>
+									</a>
+									<a href="https://twitter.com/" target='_blank' className="d-flex align-items-center justify-content-center">
+										<span className="fa-brands fa-twitter"><i className="sr-only">Twitter</i></span>
+									</a>
 								</p>
 							</div>
-							<div className="col-md-2 text-center p-3">
-								<Link className="navbar-brand" to="/"><img className="navLogo" src={"/final-logo.png"} alt='Jassar enterprise'></img></Link>
+							<div className="col-md-2 text-center p-3 col-6">
+								<Link className="navbar-brand" to="/">
+									<img className="navLogo" src={"/final-logo.png"} alt='Jassar enterprise'></img>
+								</Link>
 							</div>
-							<div className="col-md-5 d-md-flex justify-content-end mb-md-0  align-items-center">
-
-
-								<div className=" w-100 text-center ">
-									<div className="icon d-flex align-items-center justify-content-center">
-										<span className="fa fa-phone"></span>
+							<div className="col-md-5 col-3 d-flex justify-content-end mb-md-0 align-items-center">
+								<div className='d-none d-lg-flex'>
+									<div className="w-100 text-center">
+										<div className="icon d-flex align-items-center justify-content-center">
+											<span className="fa fa-phone"></span>
+										</div>
+										<div className="text">
+											<p><a href="tel://1234567920">+1 778 929 8020</a></p>
+										</div>
 									</div>
-									<div className="text">
-										<p><a href="tel://1234567920">+1 778 929 8020</a></p>
-									</div>
-								</div>
-
-
-								<div className=" w-100 text-center">
-									<div className="icon d-flex align-items-center justify-content-center">
-										<span className="fa fa-paper-plane"></span>
-									</div>
-									<div className="text">
-										<p> <a href="mailto:info@yoursite.com">	info@jassarlandscaping.ca</a></p>
+									<div className="w-100 text-center">
+										<div className="icon d-flex align-items-center justify-content-center">
+											<span className="fa fa-paper-plane"></span>
+										</div>
+										<div className="text">
+											<p><a href="mailto:info@yoursite.com">info@jassarlandscaping.ca</a></p>
+										</div>
 									</div>
 								</div>
-
 							</div>
 						</div>
 					</div>
 					<div className="col-md-4 d-flex">
-
 					</div>
 				</div>
 			</div>
 			<nav className="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
 				<div className="container-fluid">
-					<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-						<span className="fa fa-bars"></span> Menu
-					</button>
+
 					<div className="collapse navbar-collapse" id="ftco-nav">
 						<ul className="navbar-nav m-auto">
 							<li className="nav-item">
@@ -97,11 +101,42 @@ const Navbar = () => {
 								<Link to="/contact" className="nav-link">Contact</Link>
 							</li>
 						</ul>
+						<div className="social-media d-flex align-items-center d-lg-none ">
+							<p className="mb-0 d-flex">
+								<a href="https://www.facebook.com/profile.php?id=100063689943465" target='_blank' className="d-flex align-items-center justify-content-center mr-4">
+									<span className="fa-brands fa-facebook"><i className="sr-only">Facebook</i></span>
+								</a>
+								<a href="https://www.instagram.com/jassar_ent_landscping/" target='_blank' className="d-flex align-items-center justify-content-center mr-4">
+									<span className="fa-brands fa-instagram"><i className="sr-only">Instagram</i></span>
+								</a>
+								<a href="https://twitter.com/" target='_blank' className="d-flex align-items-center justify-content-center">
+									<span className="fa-brands fa-twitter"><i className="sr-only">Twitter</i></span>
+								</a>
+							</p>
+						</div>
+						<div className="d-flex justify-content-center mt-3 d-lg-none">
+							<div className="text-center mx-3">
+								<div className="icon d-flex align-items-center justify-content-center">
+									<span className="fa fa-phone"></span>
+								</div>
+								<div className="text">
+									<p><a href="tel://1234567920">+1 778 929 8020</a></p>
+								</div>
+							</div>
+							<div className="text-center mx-3">
+								<div className="icon d-flex align-items-center justify-content-center">
+									<span className="fa fa-paper-plane"></span>
+								</div>
+								<div className="text">
+									<p><a href="mailto:info@yoursite.com">info@jassarlandscaping.ca</a></p>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</nav>
 		</>
-	)
-}
+	);
+};
 
-export default Navbar
+export default Navbar;
